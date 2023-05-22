a29994ae5a284a0d741c2b1238275aa77f4d64ee
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-05-22 10:56:02 +0200

    Update Hearder.js

--- src/components/firstlayout.js
+++ src/components/firstlayout.js
@@ -1,6 +1,11 @@
     // import Routine from '../images/Routine.png'
     // import skincare from '../images/skincare.png'
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
+import React from 'react';
+//import { Badge } from "@material-ui/core";
+//import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+// import { mobile } from "../responsive";
+
 const Firstlayout = () =>{
     return (
     <>
