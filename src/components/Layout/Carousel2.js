adfd9664f17aebf63649124e22f825d5117753a9
Matthieu B <mesjeux0108@gmail.com>
2023-06-05 20:07:06 +0200

    composant carousel

---
+++ src/components/Layout/Carousel2.js
@@ -0,0 +1,39 @@
+
+import { useState,useEffect } from "react";
+import point from "../../images/point.png";
+const Carousel2 =({props})=>{
+    // props : {{items,nbShow}}
+    const [show,setShow]= useState(0)
+    const [items,setItems] = useState()
+    useEffect(() => {
+        let i;
+        let list = [];
+        for (i=show;i<props.nbShow+show;i++){
+            
+            console.log(i>=Object.keys(props.items).length,Object.keys(props.items).length,i)
+            if(i>=Object.keys(props.items).length){
+                list.push(props.items[i-Object.keys(props.items).length])
+            }else{
+                list.push(props.items[i])
+            }
+        }
+        setItems(list)
+    }, [props.items,show])
+    
+    return (
+        <div className="flex flex-row w-full">
+            <div className="w-[15%] flex center">
+                <p className="text-5xl" onClick={()=>setShow(show == 0 ? Object.keys(props.items).length :show-1)}>{"<"}</p>
+            </div>
+            <div className="flex flex-row w-[70%] h-full justify-between ">
+                
+                {items}
+            </div>
+            <div className="w-[15%] flex center">
+                <p className="text-5xl" onClick={()=>setShow(show>=Object.keys(props.items).length ? 1:show+1)}>{">"}</p>
+            </div>
+        </div>
+        
+    )
+}
+export default Carousel2
--- src/pages/Commu.js
+++ src/pages/Commu.js
@@ -4,10 +4,36 @@ import Layout1image from "../components/Layout/Layout1image";
 import Layout1imageinv from "../components/Layout/Layout1imageinv";
 import LayoutFullImage from "../components/Layout/LayoutFullImage";
 import { switchtext } from "../components/textdivers";
+import v15 from "./../images/visage15.png";
+import v16 from "./../images/visage16.png";
 import v17 from "./../images/visage17.png";
 import v18 from "./../images/visage18.png";
 import v19 from "./../images/visage19.png";
+import v14 from "./../images/visage14.png";
+import v2 from "./../images/visage2.png";
+import v3 from "./../images/visage3.png";
+import v4 from "./../images/visage4.png";
+import Carousel2 from './../components/Layout/Carousel2';
 const Commu = ()=>{
+    function itemCarousel(image,titre){
+        return (<div className="relative w-[300px] h-[200px] rounded-2xl bg-white" key={`item-${titre}`}>
+            <img src={image} alt={titre} className="w-full h-full rounded-2xl"/>
+            <div className="absolute bottom-0 w-full h-[30px] text-white text-lg bg-black opacity-50 rounded-b-2xl">
+                {titre}
+            </div>
+        </div>)
+
+    }
+    let listCarousel = [
+        itemCarousel(v2,"visage2"),
+        itemCarousel(v3,"visage3"),
+        itemCarousel(v4,"visage4"),
+        itemCarousel(v14,"visage14"),
+        itemCarousel(v15,"visage15"),
+        itemCarousel(v16,"visage16"),
+        itemCarousel(v19,"visage19")
+        
+    ]
     return (<>
         <LayoutFullImage props={{titre:"MA COMMUNAUTÉ",image1:{url:v17,alt:"visage17"}}}/>
         <div className="w-full flex center mt-[100px]">
@@ -23,9 +49,11 @@ const Commu = ()=>{
                 <div className="w-[1100px] h-[220px] bg-white rounded-r-3xl p-4">
                     <p className="text-[36px] font-av-bold text-left">Nouveau venu ?</p>
                 </div>
-            </div>
+                </div>
             </div>
         </div>
+        <Carousel2 props={{items:listCarousel,nbShow:4}}/>
+
         </>
     )
 }
