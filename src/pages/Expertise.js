47f0acba500b857b7eda8908e89aefd09575a970
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-06-12 03:14:38 +0200

    Update Expertise.js

--- src/pages/Expertise.js
+++ src/pages/Expertise.js
@@ -1,1 +1,21 @@
-import Firstlayout from "../components/firstlayout";
+
+import ExpertiseHome from "../components/ExpertiseHome";
+import Layout1image from "../components/Layout/Layout1image";
+import Layout1imageinv from "../components/Layout/Layout1imageinv";
+import LayoutFullImage from "../components/Layout/LayoutFullImage";
+import { switchtext } from "../components/textdivers";
+import v13 from "./../images/visage/visage13.png";
+import v14 from "./../images/visage/visage14.png";
+import v15 from "./../images/visage/visage15.png";
+import v16 from "./../images/visage/visage16.png";
+const Expertise = ()=>{
+    return (<>
+        <LayoutFullImage props={{titre:"NOTRE EXPERTISE",image1:{url:v13,alt:"visage13"}}}/>
+        <Layout1image props={{col1:switchtext("NotreHistoire"),image1:{url:v14,alt:"visage14"}}} />
+        <ExpertiseHome/>
+        <Layout1imageinv props={{col1:switchtext("EcoleDelphine"),image1:{url:v15,alt:"visage15"}}}/>
+        <Layout1image props={{col1:switchtext("Une expertise, une passion"),image1:{url:v16,alt:"visage16"}}} />
+        </>
+    )
+}
+export default Expertise;
